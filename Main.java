240751c044066a9376623c6112b62f58dbc92335
fmatoruga2 <72858442+fmatoruga2@users.noreply.github.com>
2023-10-17 16:46:58 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+      
+        Scanner in = new Scanner(System.in);
+	   System.out.println("Prvi broj: ");
+        int prvi=in.nextInt();
+	   System.out.println("Drugi broj: ");
+        int drugi=in.nextInt();
+        System.out.println("Dva broja su: " + prvi + " i " + drugi + " .");
+
+    }
+}
